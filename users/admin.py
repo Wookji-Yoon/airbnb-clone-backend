79437ab04b6badf50bcab25853745970df00c851
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-08 15:21:30 +0900

    5.4 Custom Admin

--- users/admin.py
+++ users/admin.py
@@ -7,4 +7,31 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "Profile",
+            {"fields": ("username", "password", "name", "email", "is_host")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collaspe"),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collaspe"),
+            },
+        ),
+    )
+
+    list_display = ("username", "email", "name", "is_host")
--- users/models.py
+++ users/models.py
@@ -1,6 +1,18 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+
+    # 기존의 default Django User Model 중 일부 property를 무력화함
+    # editable=False를 활용하여, '얘는 쓰지마'라고 하는 거임
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+
+    # email 기준으로 계정관리하기 위헤 email을 unique
+    email = models.EmailField(unique=True)
+
+    # 기존 user data에 추가할 때 오류 방지 위해, default 넣어줌
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
