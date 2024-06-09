611156325b8186f6b8469f09d852f1b8a7a902ce
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-10 00:03:08 +0900

    6.3 Recap

--- rooms/models.py
+++ rooms/models.py
@@ -30,4 +30,4 @@ class Amenity(CommonModel):
     """Amenity Definition"""
 
     name = models.CharField(max_length=150)
-    description = models.CharField(max_length=150, null=True)
+    description = models.CharField(max_length=150, null=True, blank=True)
