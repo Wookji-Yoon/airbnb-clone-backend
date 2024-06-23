4dad2180316de965d064b9fdffcbfec690787743
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-23 15:25:11 +0900

    #9.2 URL Arguments

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("all", views.see_all_room),
+    # 파라미터를 넣는 방법은 아래와 같다.
+    path("<int:room_id>", views.see_one_room),
 ]
