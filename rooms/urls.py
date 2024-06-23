22654438d15fe9db1059f85a341176ab885d040f
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-23 15:51:19 +0900

    #9.5 DoeeNotExist

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.see_all_room),
     # 파라미터를 넣는 방법은 아래와 같다.
-    path("<int:room_id>", views.see_one_room),
+    path("<int:room_pk>", views.see_one_room),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -5,7 +5,7 @@ from .models import Room
 
 # Create your views here.
 def see_all_room(request):
-    # objects는 모델과 소통할 수 있게 하는 API 매니저
+    # objects는 모델과 데이터베이스가 소통(ORM)할 수 있게 하는 API 매니저
     rooms = Room.objects.all()
 
     # render의 context argument는 html에 변수를 넘기는 것
@@ -20,5 +20,19 @@ def see_all_room(request):
     )
 
 
-def see_one_room(request, room_id):
-    return HttpResponse(room_id)
+def see_one_room(request, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        return render(
+            request,
+            "room_detail.html",
+            context={"room": room},
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room_detail.html",
+            context={
+                "not_found": True,
+            },
+        )
