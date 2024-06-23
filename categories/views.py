df32cff1c3abb0076d6e2c0f3cde533ee4653313
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-23 22:17:44 +0900

    #10.4 POST Requests

--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import Category
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CategorySerializer
 
 
-# Create your views here.
+# api_view를 데코레이터로 붙이고, Reponse를 레스트프레임워크 리스폰스로 하면 된다.
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse({"ok": True})
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response({"ok": True, "cateogries": serializer.data})
+    elif request.method == "POST":
+        return Response({"created": True})
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response({"category": serializer.data})
