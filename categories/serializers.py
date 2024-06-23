6614052970cdf917f0d370798a227ea1ff02c88a
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-23 22:42:12 +0900

    #10.6 save

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from .models import Category
+
+""" 5. serializer를 한번만 만들어두면 객체가 django모델에서 user쪽에서 원하는 것으로 번역해줄수 있다
+6. serializer에게 user가 created_at과 pk는 보내지 않는다고 말하려면 read_only = True
+7. serializer에게 어떤게 필요한지만 알려주면 된다 그럼 모델을 django에서 json으로 변역해줄거고 검증을 거쳐서 user데이터로 부터 모델을 만들수 있게해준다 """
 
 
 class CategorySerializer(serializers.Serializer):
 
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, max_length=50)
+    kind = serializers.ChoiceField(choices=Category.CategoryKindChoices.choices)
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validate_data):
+        return Category.objects.create(**validate_data)
--- categories/views.py
+++ categories/views.py
@@ -10,10 +10,16 @@ from .serializers import CategorySerializer
 def categories(request):
     if request.method == "GET":
         all_categories = Category.objects.all()
+        # Serializer는 Quesyset을 JSON으로 바꾸어준다.
         serializer = CategorySerializer(all_categories, many=True)
         return Response({"ok": True, "cateogries": serializer.data})
     elif request.method == "POST":
-        return Response({"created": True})
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"created": True})
+        else:
+            return Response(serializer.errors)
 
 
 @api_view()
