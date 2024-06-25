f99f164606225112c041f1f40154abe0771f1427
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-25 23:44:44 +0900

    #11.6 Room owner

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, NotAuthenticated
 from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Amenity, Room
 from .serializers import AmenitySerializer, RoomDetailSerializer, RoomListSerializer
@@ -62,12 +62,26 @@ class AmenityDetail(APIView):
 class Rooms(APIView):
 
     def get(self, request):
-        all_data = Room.objects.all()
+        all_items = Room.objects.all()
         serializer = RoomListSerializer(
-            all_data,
+            all_items,
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            serializer = RoomDetailSerializer(data=request.data)
+            if serializer.is_valid():
+                # 만약 valid를 통과했다 하더라도, save단계에서 nested field는 serializer을 통해 default로 생성하지 못한다. 그렇다면 관계를 serializer에 explicit하거나 read_only=True를 해야 한다.
+                #
+                new_item = serializer.save()
+
+                return Response(RoomDetailSerializer(new_item).data)
+            else:
+                return Response(serializer.errors)
+        else:
+            raise NotAuthenticated
 
 
 class RoomDetail(APIView):
