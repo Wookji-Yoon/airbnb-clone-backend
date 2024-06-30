403c60212b5fa269158e0bdf8317cfa0eb3c6f7c
Wookji-Yoon <wookji.yoon@gmail.com>
2024-06-30 16:00:31 +0900

    직접 Refactoring

--- rooms/views.py
+++ rooms/views.py
@@ -75,33 +75,47 @@ class Rooms(APIView):
         if request.user.is_authenticated:
             serializer = RoomDetailSerializer(data=request.data)
             if serializer.is_valid():
-                # nested field에 해당하는 애들을 serializer로 valid할 수는 있다. 그런데, 이것을 곧바로 save한다고 알맞게 data를 생성하지 못한다.
-                # 따라서 두 가지 조치가 필요하다.
-                # 1. save단계에서 유저가 보낸 data가 곧바로 넘어가지 못하도록 serializer에 read_only = True를 걸어야 한다.
-                # 2. save의 parameter로 관계를  explicit해야 한다.
+                # 아래부터의 각주와 코드는 #11.9 강의와 상관없이 내가 Refactoring한 것이다.
+
+                # request.data에서 nested field의 pk를 받았을 때, serializer.is_valid()로 유효성 검사는 가능합니다. 하지만,
+                # serializer.save()를 바로 사용하면 올바른 데이터 생성이 어렵습니다. 왜냐하면, 실제 저장해야 할 데이터는 pk가 아닌 모델의 인스턴스이기 때문입니다.
+                # 따라서, pk로 해당 모델의 인스턴스를 찾아 serializer.save()에 인자로 전달해야 합니다.
+
+                # A. owner: User 모델과 ForeignKey로 연결됩니다. request에서 찾습니다.
+                owner = request.user
+
+                # B. category: Category 모델과 ForeignKey로 연결됩니다. 'Rooms' 종류의 카테고리만 유효합니다.
                 category_pk = request.data.get("category")
                 if category_pk is None:
                     raise ParseError("Category pk is required")
                 try:
                     category = Category.objects.get(pk=category_pk)
                     if category.kind != Category.CategoryKindChoices.ROOMS:
-                        raise ParseError("Cateogry shoud be 'rooms'")
-                except category.DoesNotExist:
-                    raise ParseError("Cannot find Cateogry")
+                        raise ParseError("Category should be 'Rooms'")
+                except Category.DoesNotExist:
+                    raise ParseError("Cannot find Category")
 
-                with transaction.atomic():
-
-                    new_item = serializer.save(
-                        owner=request.user,
-                        category=category,
-                    )
-
-                    amenities = request.data.get("amenities")
-                    for amenity_pk in amenities:
+                # C. amenities: Amenity 모델과 ManyToManyField로 연결됩니다. 유효한 Amenity의 pk 리스트를 받습니다.
+                pk_list = request.data.get("amenities")
+                amenities = []
+                for amenity_pk in pk_list:
+                    try:
                         amenity = Amenity.objects.get(pk=amenity_pk)
-                        new_item.amenities.add(amenity)
+                        amenities.append(amenity)
+                    except Amenity.DoesNotExist:
+                        raise ParseError("Cannot find Amenity")
 
-                    return Response(RoomDetailSerializer(new_item).data)
+                # Nested Field에 해당하는 모델 인스턴스를 모두 가져왔으니, 이제 serializer.save()에 인자로 전달합니다.
+                # 사용자 입력이 있더라도, owner, category, amenities는 여기서 명시적으로 지정된 값으로 덮어쓰기 됩니다.
+                # 이는 악의적/잘못된 사용자 데이터가 최종 데이터에 영향을 미치지 않도록 보장합니다.
+                # 그러나 API 문서의 명확성을 위해, 이 필드들을 serializer에서 read_only=True로 설정하는 것이 좋습니다.
+                new_item = serializer.save(
+                    owner=request.user,
+                    category=category,
+                    amenities=amenities,
+                )
+
+                return Response(RoomDetailSerializer(new_item).data)
             else:
                 return Response(serializer.errors)
         else:
